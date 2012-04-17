eafe3ef066a0dd9deff703082ac5c4947859a266
Ross Hadden <rosshadden@gmail.com>
2012-04-17 15:41:33 -0400

    Started room views.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 exports.index = function(request, response){
+	response.render('index', {
+		title:	'Engine'
+	});
+};
+
+exports.room = function(request, response){
 	if(/^\d$/.test(request.params.room)){
 		console.log('room', request.params.room);
 	}
 	
+	console.log('room');
+	
 	response.render('index', {
-		title:	'Engine'
+		title:	'ROOM'
 	});
 };
