f41311499fcc2d9a271212813fc9267f0473d04e
Ross Hadden <rosshadden@gmail.com>
2012-04-17 01:16:14 -0400

    Starting to work on room routes.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
-exports.index = function(req, res){
-	res.render('index', {
+exports.index = function(request, response){
+	if(/^\d$/.test(request.params.room)){
+		console.log('room', request.params.room);
+	}
+	
+	response.render('index', {
 		title:	'Engine'
-	})
+	});
 };
