5fe9718717b0e7269e54311e97d989dc734e58e3
Ross Hadden <rosshadden@gmail.com>
2012-04-18 13:12:44 -0400

    Configurable paths to /engine and /entities.

--- ink/js/main.js
+++ ink/js/main.js
@@ -1,8 +1,35 @@
 require.config({
-	baseUrl:	'../'
+	paths:	{
+		'engine':	'/engine',
+		'entities':	'/entities'
+	}
 });
 
-require(['jquery', 'js/game'], function($, game){
-	//TEMP:	For debugging and experimentation.
-	window.game = game;
+require(['jquery'], function($){
+	var route = (location.pathname.match(/\/\w*/) || ['/'])[0];
+	
+	console.log('route', route);
+	
+	switch(route){
+		case '/':
+			break;
+			
+		case '/room':
+			require(['room'], function(room){
+				console.log(room);
+			});
+			break;
+			
+		case '/game':
+			require(['game'], function(game){
+				//TEMP:	For debugging and experimentation.
+				window.game = game;
+				
+				game.start();
+			});
+			break;
+			
+		default:
+			console.log('route', 'default');
+	}
 });
