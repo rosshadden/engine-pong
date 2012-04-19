fef2caded6721b9dd29e6f0f76193f7cab836742
Ross Hadden <rosshadden@gmail.com>
2012-04-19 15:47:01 -0400

    Upgraded to Express 3.

--- app.js
+++ app.js
@@ -1,109 +1,135 @@
 ////////////////////////////////////////////////////////////////
 //	MODULES
-var	express = require('express'),
+var	PORT = +(process.argv[2] || process.env.PORT || 3000),
+	express = require('express'),
 	routes = require('./routes'),
+	app = express(),
+	server = require('http').createServer(app);
 	
-	app = module.exports = express.createServer(),
-	engine = require('./engine/engine.js')(app);
+	app.io = require('socket.io').listen(server);
+	
+var	engine = require('./engine/engine.js')(app);
 
 ////////////////////////////////////////////////////////////////
 //	GAME STUFF
 	var players = {},
-		rooms = [];
+		rooms = [],
+		numPlayers = 0;
 
 ////////////////////////////////////////////////////////////////
 //	CONFIG
 app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(express.session({secret: 'Shh... it is a secret!'}));
+	app.use(express.favicon());
+	//app.use(express.logger('dev'));
 	app.use(require('stylus').middleware({
 		src:	__dirname + '/ink'
 	}));
-	app.use(app.router);
-	app.use('/engine', express.static(__dirname + '/engine/client'));
 	app.use(express.static(__dirname + '/ink'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser("Shhh... it's a secret!"));
+	app.use(express.session());
+	app.use(app.router);
+	
+	app.io.set('log level', 1);
+	
+	//TODO:	Move this to engine.js.
+	app.use('/engine', express.static(__dirname + '/engine/client'));
 });
 
 app.configure('development', function(){
-	app.use(express.errorHandler({
-		dumpExceptions:	true,
-		showStack:		true
-	}));
-});
-
-app.configure('production', function(){
 	app.use(express.errorHandler());
 });
 
 ////////////////////////////////////////////////////////////////
 //	ROUTES
-app.get('/', routes.index);
+app.get('/', function(request, response, next){
+	//console.log('session', request.user);
+	
+	next();
+}, routes.index);
 
-app.get('/host', function(request, response, next){
-	var room;
-	
-	for(room = 0 ;; room++){
-		if(!rooms[room] || rooms[room].players === 0){
-			break;
-		}
-	}
-	
-	if(!rooms[room]){
-		rooms[room] = {
-			id:			room,
-			players:	0
-		};
-	}
-	
-	response.redirect('/room/' + room);
+app.get('/engine/*', function(request, response, next){
+	next();
 });
 
-app.get('/room/:room([0-9]+)', function(request, response, next){
-	var room = request.params.room;
-	
-	if(rooms[room].players < 2){
-		rooms[room].players += 1;
+app.get('/maps/:path', function(request, response){
+	try{
+		var map = require('../../resources/maps/' + request.params.path + '.json');
 		
-		next();
-	}else{
-		response.redirect('/');
+		response.json(map);
+	}catch(e){
+		console.log('Error: A client tried to access map "%s".', request.params.path);
+		response.send('The map you seek does not exist.', 404);
 	}
-	
-	console.log(room, rooms[room]);
-});
-
-app.get('/room/:room([0-9]+)', routes.room);
-
-app.get('/game', routes.game);
-
-app.get('/get/rooms', function(request, response){
-	response.json(rooms);
 });
 
 ////////////////////////////////////////////////////////////////
 //	RUN
-app.listen(+(process.argv[2] || process.env.PORT || 3000));
+server.listen(PORT);
 
-console.log("%s:%d [%s]", app.address().address, app.address().port, app.settings.env);
+console.log("Server started on port %d [%s]", PORT, app.settings.env);
 
 ////////////////////////////////////////////////////////////////
 //	SERVE
-engine.network.start(function(socket){
+/*app.io.sockets.on('connection', function(socket){
 	var id = socket.id;
-
+	
+	console.log(id in players);
 	players[id] = {
+		id:		id,
 		socket:	socket
 	};
 	
-	app.get('/test', function(request, response){
-		response.redirect('/game');
+	console.log('Player #%d connected.', ++numPlayers);
+	
+	//	ROUTES
+	app.get('/host', function(request, response, next){
+		var room;
+		
+		for(room = 0 ;; room++){
+			if(!rooms[room] || rooms[room].players === 0){
+				break;
+			}
+		}
+		
+		if(!rooms[room]){
+			rooms[room] = {
+				id:			room,
+				players:	0
+			};
+		}
+		
+		response.redirect('/room/' + room);
+	});
+
+	app.get('/room/:room([0-9]+)', function(request, response, next){
+		var room = request.params.room;
+		
+		if(rooms[room].players < 2){
+			rooms[room].players += 1;
+			
+			socket.join('room-' + room);
+			
+			console.log('Player %s joined room #%d.', id, room);
+			
+			next();
+		}else{
+			response.redirect('/');
+		}
+	}, routes.room);
+	
+	app.get('/game', routes.game);
+	
+	//	AJAX
+	app.get('/get/rooms', function(request, response){
+		response.json(rooms);
 	});
 	
 	socket.on('move', function(y){
 		socket.broadcast.emit('move', y);
 	});
 });
+*/
